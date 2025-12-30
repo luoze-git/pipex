acb26cf459c9d442adaca7cdeb6e4beeff368638
luozguo <anitadou9@gmail.com>
2025-12-30 15:53:01 +0100

    false idea about error design is found. rescan now

--- main.c
+++ main.c
@@ -6,11 +6,11 @@ int	main(int argc, char **argv, char **envp)
 	t_parent	parent;
 	int			exit_code;
 
-	parsed = parse_input(argc, argv, envp);//todo maclloc
+	parsed = parse_input(argc, argv, envp);//todo guard
 	parent.parsed = parsed;
 	parent.pipes = setup_pipes(parsed->cmd_count);
 	parent.pids = NULL;
-	launch_pipeline(&parent, envp);
+	launch_pipeline(&parent, envp); // todo check error before running execve for children
 	close_all_fd(&parent);
 	exit_code = parent_wait_and_collect(&parent);
 	cleanup_parent(&parent);//todo
