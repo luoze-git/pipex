c7df7b2f321f42edd1c4039c492053409d649950
luozguo <anitadou9@gmail.com>
2025-12-30 15:32:52 +0100

    guard for double close and faulty dereferencing

--- psupervise.c
+++ psupervise.c
@@ -4,14 +4,25 @@ void	close_all_fd(t_parent *parent)
 {
 	int	i;
 
-	close(parent->parsed->in_fd);
-	close(parent->parsed->out_fd);
+    if (!parent || !parent->parsed)
+        return;
+    if (parent->parsed->in_fd >= 0)
+        close(parent->parsed->in_fd);
+    if (parent->parsed->out_fd >= 0)
+        close(parent->parsed->out_fd);
 	i = 0;
+    if (!parent->pipes)
+        return;
 	while (i < parent->parsed->cmd_count - 1)
 	{
-		close(parent->pipes[i][0]);
-		close(parent->pipes[i][1]);
-		i++;
+        if (parent->pipes[i])
+        {
+        if (parent->pipes[i][0] >= 0)
+            close(parent->pipes[i][0]);
+        if (parent->pipes[i][1] >= 0)
+            close(parent->pipes[i][1]);
+        }
+        i++;
 	}
 }
 
@@ -24,7 +35,7 @@ int	wait_status_to_exit_code(int status)
 	else
 		return (1);
 }
-// read
+
 int	parent_wait_and_collect(t_parent *parent)
 {
 	int	i;
